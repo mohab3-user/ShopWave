9ff5a60da904202850fb7f8f46bbf57334619c66
Mohab <mohabnayel3@gmail.com>
2026-05-13 00:03:41 +0300

    Configure for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -117,6 +117,10 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production' || !process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
